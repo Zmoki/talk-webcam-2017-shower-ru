70fc5e2d067ff829e73451b34ea52d1518fdda72
Zarema Khalilova <zarema.khalilova@gmail.com>
2017-09-09 10:26:10 +0300

    Add setInterval to demo canvas

--- assets/scripts/demo-canvas.js
+++ assets/scripts/demo-canvas.js
@@ -7,6 +7,8 @@ window.addEventListener('popstate', () => {
     const $preview = $parent.querySelector('.demo-preview')
     const $canvas = $parent.querySelector('.demo-canvas')
 
+    let frameInterval
+    let frameDrawed
     const constraints = {
       video: true,
       audio: false,
@@ -48,17 +50,27 @@ window.addEventListener('popstate', () => {
               context.drawImage(img, 80, 80, 250, 300)
             }
 
-            requestAnimationFrame(draw)
+            frameDrawed = true
           }
 
-          requestAnimationFrame(draw)
+          const {frameRate} = (stream.getVideoTracks()[0]).getSettings()
+
+          frameDrawed = true
+          frameInterval = setInterval(() => {
+            if (!frameDrawed)
+              return
+
+            frameDrawed = false
+            requestAnimationFrame(draw)
+          }, Math.round(1000 / frameRate))
 
           $preview.hidden = true
           $canvas.hidden = false
         }
 
         window.addEventListener('popstate', () => {
-          (stream.getVideoTracks()[0]).stop()
+          clearInterval(frameInterval)
+          ;(stream.getVideoTracks()[0]).stop()
 
           $preview.hidden = false
           $canvas.hidden = true
